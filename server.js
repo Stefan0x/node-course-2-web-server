1cf6202adadcf8de38d23ca6d7cd16770fdd6004
Stefan Test <stefantest@MacBook-Pro-2.fritz.box>
2017-09-26 08:28:12 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+// Templating engine: http://handlebarsjs.com/
+app.set('view engine', 'hbs');
+
+// Middleware
+app.use((req, res, next) =>{
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) =>{
+//   res.render('maintenance.hbs');
+//   // Weil next() fehlt, hört die App hier auf
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Homepage',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Welcome to BeYou.com'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Error handling request.'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up and running!');
+});
